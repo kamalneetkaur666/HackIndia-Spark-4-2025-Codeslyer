b62d30683f9e1e270df7260c637a595cf3099839
kamalneetkaur666 <kamalneetkaur4@gmail.com>
2025-04-01 22:26:34 +0530

    first commit

---
+++ nftMarketplace-subscribe-main/pages/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+//INTERNAL IMPORT
+import Style from "../styles/index.module.css";
+import {
+  HeroSection,
+  Service,
+  BigNFTSilder,
+  Subscribe,
+} from "../components/componentsindex";
+
+const Home = () => {
+  return (
+    <div className={Style.homePage}>
+      <HeroSection />
+      <Service />
+      <BigNFTSilder />
+      <Subscribe />
+    </div>
+  );
+};
+
+export default Home;
